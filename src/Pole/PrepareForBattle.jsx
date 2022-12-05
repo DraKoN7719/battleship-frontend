83a23709311c1286fd5d957eb2e2ac29ef35e384
groute63 <startref@yandex.ru>
2022-12-05 12:43:45 +0400

    delete Ship

--- src/Pole/PrepareForBattle.jsx
+++ src/Pole/PrepareForBattle.jsx
@@ -17,23 +17,23 @@ const PrepareForBattle = function () {
     ]);
     const [modalActive, setModalActive] = useState(false);
     const [coordinates, setCoordinates] = useState([
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     ]);
     const [invertedShip, setInvertedShip] = useState(false)
     const [isBattleThePlayer, setIsBattleThePlayer] = useState(false) // проверка на игру с игроком
     const [countOrientation, setCountOrientation] = useState(0)
     const [selectedShip, setSelectedShip] = useState()
     const [selectedSquare, setSelectedSquare] = useState() // координаты куда пользователь пытается поставить корабль
-    const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
+    const [mousePos, setMousePos] = useState({x: 0, y: 0});
     const [activeShip, setActiveShip] = useState() // какой корабль взял пользователь
     const handleMouse = (event) => {
         const mouse = {};
@@ -43,56 +43,60 @@ const PrepareForBattle = function () {
     };
 
     function getShip(e) {
-        if(selectedShip !== undefined && shipList[selectedShip-1].count > 0 && e.button === 0){
+        if (selectedShip !== undefined && shipList[selectedShip - 1].count > 0 && e.button === 0) {
             let copy = Object.assign([], shipList);
-            copy[selectedShip-1] = {id: selectedShip, numberOfDecks: shipList[selectedShip-1].numberOfDecks, count: shipList[selectedShip-1].count - 1};
+            copy[selectedShip - 1] = {
+                id: selectedShip,
+                numberOfDecks: shipList[selectedShip - 1].numberOfDecks,
+                count: shipList[selectedShip - 1].count - 1
+            };
             setShipList(copy);
             setActiveShip(selectedShip);
         }
     }
 
     function check(shipId) {
-        let horizontalShip = [0,0,0];
-        let verticalShip = [0,0,0];
+        let horizontalShip = [0, 0, 0];
+        let verticalShip = [0, 0, 0];
         let check = true;
-        invertedShip ? verticalShip = [1,2,3]: horizontalShip = [1,2,3];
-        invertedShip && (selectedSquare.y + shipList[shipId-1].numberOfDecks - 1 <= 9 ? check = true: check = false);
-        if(coordinates[selectedSquare.y][selectedSquare.x] === 0){
+        invertedShip ? verticalShip = [1, 2, 3] : horizontalShip = [1, 2, 3];
+        invertedShip && (selectedSquare.y + shipList[shipId - 1].numberOfDecks - 1 <= 9 ? check = true : check = false);
+        if (coordinates[selectedSquare.y][selectedSquare.x] === 0) {
             switch (shipId) {
-                case 1:{
-                    if(check && coordinates[selectedSquare.y+verticalShip[0]][selectedSquare.x+horizontalShip[0]] === 0 &&
-                        coordinates[selectedSquare.y+verticalShip[1]][selectedSquare.x+horizontalShip[1]] === 0 &&
-                        coordinates[selectedSquare.y+verticalShip[2]][selectedSquare.x+horizontalShip[2]] === 0){
+                case 1: {
+                    if (check && coordinates[selectedSquare.y + verticalShip[0]][selectedSquare.x + horizontalShip[0]] === 0 &&
+                        coordinates[selectedSquare.y + verticalShip[1]][selectedSquare.x + horizontalShip[1]] === 0 &&
+                        coordinates[selectedSquare.y + verticalShip[2]][selectedSquare.x + horizontalShip[2]] === 0) {
                         coordinates[selectedSquare.y][selectedSquare.x] = 1;
-                        coordinates[selectedSquare.y+verticalShip[0]][selectedSquare.x+horizontalShip[0]] = 1;
-                        coordinates[selectedSquare.y+verticalShip[1]][selectedSquare.x+horizontalShip[1]] = 1;
-                        coordinates[selectedSquare.y+verticalShip[2]][selectedSquare.x+horizontalShip[2]] = 1;
+                        coordinates[selectedSquare.y + verticalShip[0]][selectedSquare.x + horizontalShip[0]] = 1;
+                        coordinates[selectedSquare.y + verticalShip[1]][selectedSquare.x + horizontalShip[1]] = 1;
+                        coordinates[selectedSquare.y + verticalShip[2]][selectedSquare.x + horizontalShip[2]] = 1;
                         placementRules(4);
                         return true;
                     }
                     return false;
                 }
-                case 2:{
-                    if(check && coordinates[selectedSquare.y+verticalShip[0]][selectedSquare.x+horizontalShip[0]] === 0 &&
-                        coordinates[selectedSquare.y+verticalShip[1]][selectedSquare.x+horizontalShip[1]] === 0){
+                case 2: {
+                    if (check && coordinates[selectedSquare.y + verticalShip[0]][selectedSquare.x + horizontalShip[0]] === 0 &&
+                        coordinates[selectedSquare.y + verticalShip[1]][selectedSquare.x + horizontalShip[1]] === 0) {
                         coordinates[selectedSquare.y][selectedSquare.x] = 1;
-                        coordinates[selectedSquare.y+verticalShip[0]][selectedSquare.x+horizontalShip[0]] = 1;
-                        coordinates[selectedSquare.y+verticalShip[1]][selectedSquare.x+horizontalShip[1]] = 1;
+                        coordinates[selectedSquare.y + verticalShip[0]][selectedSquare.x + horizontalShip[0]] = 1;
+                        coordinates[selectedSquare.y + verticalShip[1]][selectedSquare.x + horizontalShip[1]] = 1;
                         placementRules(3);
                         return true;
                     }
                     return false;
                 }
-                case 3:{
-                    if(check && coordinates[selectedSquare.y+verticalShip[0]][selectedSquare.x+horizontalShip[0]] === 0){
+                case 3: {
+                    if (check && coordinates[selectedSquare.y + verticalShip[0]][selectedSquare.x + horizontalShip[0]] === 0) {
                         coordinates[selectedSquare.y][selectedSquare.x] = 1;
-                        coordinates[selectedSquare.y+verticalShip[0]][selectedSquare.x+horizontalShip[0]] = 1;
+                        coordinates[selectedSquare.y + verticalShip[0]][selectedSquare.x + horizontalShip[0]] = 1;
                         placementRules(2);
                         return true;
                     }
                     return false;
                 }
-                case 4:{
+                case 4: {
                     coordinates[selectedSquare.y][selectedSquare.x] = 1;
                     placementRules(1);
                     return true;
@@ -104,26 +108,47 @@ const PrepareForBattle = function () {
 
     function placementRules(numberOfDecks) {
         const coordinates2 = coordinates.slice();
-        if(invertedShip){
-            for (let x = selectedSquare.x - 1, y = selectedSquare.y - 1; y < selectedSquare.y+numberOfDecks+1; x++){
-                if(x < 0) continue;
-                else if(y < 0) {y++; x-=1; continue;}
-                else if(x > 9) {y++; x=selectedSquare.x - 2; continue;}
-                else if(y === 10) {break;}
-                if(coordinates2[y][x] !== 1 && coordinates2[y][x] < 2) coordinates2[y][x] = 2;
-                else if(coordinates2[y][x] >= 2) coordinates2[y][x] += 2;
-                if(x === selectedSquare.x+1) {x = selectedSquare.x - 2; y++;}
+        if (invertedShip) {
+            for (let x = selectedSquare.x - 1, y = selectedSquare.y - 1; y < selectedSquare.y + numberOfDecks + 1; x++) {
+                if (x < 0) continue;
+                else if (y < 0) {
+                    y++;
+                    x -= 1;
+                    continue;
+                } else if (x > 9) {
+                    y++;
+                    x = selectedSquare.x - 2;
+                    continue;
+                } else if (y === 10) {
+                    break;
+                }
+                if (coordinates2[y][x] !== 1 && coordinates2[y][x] < 2) coordinates2[y][x] = 2;
+                else if (coordinates2[y][x] >= 2) coordinates2[y][x] += 2;
+                if (x === selectedSquare.x + 1) {
+                    x = selectedSquare.x - 2;
+                    y++;
+                }
             }
-        }
-        else {
-            for (let x = selectedSquare.x - 1, y = selectedSquare.y - 1; x < selectedSquare.x+numberOfDecks+1; y++){
-                if(y < 0) continue;
-                else if(x < 0) {x++; y-=1; continue;}
-                else if(y > 9) {x++; y=selectedSquare.y - 2; continue;}
-                else if(x === 10) {break;}
-                if(coordinates2[y][x] !== 1 && coordinates2[y][x] < 2) coordinates2[y][x] = 2;
-                else if(coordinates2[y][x] >= 2) coordinates2[y][x] += 2;
-                if(y === selectedSquare.y+1) {y = selectedSquare.y - 2; x++;}
+        } else {
+            for (let x = selectedSquare.x - 1, y = selectedSquare.y - 1; x < selectedSquare.x + numberOfDecks + 1; y++) {
+                if (y < 0) continue;
+                else if (x < 0) {
+                    x++;
+                    y -= 1;
+                    continue;
+                } else if (y > 9) {
+                    x++;
+                    y = selectedSquare.y - 2;
+                    continue;
+                } else if (x === 10) {
+                    break;
+                }
+                if (coordinates2[y][x] !== 1 && coordinates2[y][x] < 2) coordinates2[y][x] = 2;
+                else if (coordinates2[y][x] >= 2) coordinates2[y][x] += 2;
+                if (y === selectedSquare.y + 1) {
+                    y = selectedSquare.y - 2;
+                    x++;
+                }
             }
         }
         setCoordinates(coordinates2);
@@ -132,12 +157,16 @@ const PrepareForBattle = function () {
 
     function releasedShip(e) {
         e.preventDefault();
-        if (activeShip !== undefined){
+        if (activeShip !== undefined) {
             let shipId = activeShip;
             setActiveShip(undefined);
-            if(selectedSquare === undefined || !check(shipId)){
+            if (selectedSquare === undefined || !check(shipId)) {
                 let copy = Object.assign([], shipList);
-                copy[activeShip-1] = {id: activeShip, numberOfDecks: shipList[activeShip-1].numberOfDecks, count: shipList[activeShip-1].count + 1};
+                copy[activeShip - 1] = {
+                    id: activeShip,
+                    numberOfDecks: shipList[activeShip - 1].numberOfDecks,
+                    count: shipList[activeShip - 1].count + 1
+                };
                 setShipList(copy);
             }
         }
@@ -157,30 +186,135 @@ const PrepareForBattle = function () {
     }, []);
 
     function orientationShip(e) {
-        if (activeShip !== undefined && e.code === "KeyR"){
-            setCountOrientation(countOrientation+1);
-            if(countOrientation === 1) {
+        if (activeShip !== undefined) {
+            setCountOrientation(countOrientation + 1);
+            if (countOrientation === 1) {
                 setInvertedShip(false);
                 setCountOrientation(0);
-            }
-            else setInvertedShip(true);
+            } else setInvertedShip(true);
         }
     }
 
+    function press(e) {
+        if (e.code === "KeyD") deleteShip();
+        if (e.code === "KeyR") orientationShip();
+    }
+
+    function deleteShip(e) {
+        let shipId = 0;
+        let x = selectedSquare.y;
+        let y = selectedSquare.x;
+        if (coordinates[x][y] === 1) {
+            coordinates[x][y] = 0;
+            shipId += 1
+            deleteArea(x, y,'all');
+            let n = 1;
+            while (checkNext(x + n, y,'down')) {
+                shipId += 1;
+                n++;
+            }
+            n = 1;
+            while (checkNext(x - n, y,'up')) {
+                shipId += 1;
+                n++;
+            }
+            n = 1;
+            while (checkNext(x, y + n,'right')) {
+                shipId += 1;
+                n++;
+            }
+            n = 1;
+            while (checkNext(x, y - n,'left')) {
+                shipId += 1;
+                n++;
+            }
+
+            switch (shipId){
+                case 4:
+                    shipList[0].count = shipList[0].count+1
+                    break
+                case 3:
+                    shipList[1].count = shipList[1].count+1
+                    break
+                case 2:
+                    shipList[2].count = shipList[2].count+1
+                    break
+                case 1:
+                    shipList[3].count = shipList[3].count+1
+                    break
+            }
+            renderShips(coordinates)
+        }
+    }
+
+    function checkNext(x, y,pos) {
+        console.log(x + " " + y)
+        if (checkBounds(x, y) && coordinates[x][y] === 1) {
+            coordinates[x][y] = 0;
+            deleteArea(x, y,pos);
+            return true
+        }
+        return false
+    }
+
+    function deleteArea(x, y,pos) {
+        console.log(pos + "  " + pos ==='right')
+        if(pos === 'right'){
+            if (checkBounds(x + 1, y + 1) && coordinates[x + 1][y + 1] >= 2) coordinates[x + 1][y + 1] -= 2;
+            if (checkBounds(x - 1, y + 1) && coordinates[x - 1][y + 1] >= 2) coordinates[x - 1][y + 1] -= 2;
+            if (checkBounds(x, y + 1) && coordinates[x][y + 1] >= 2) coordinates[x][y + 1] -= 2;
+        }
+        if(pos === 'up'){
+            if (checkBounds(x - 1, y) && coordinates[x - 1][y] >= 2) coordinates[x - 1][y] -= 2;
+            if (checkBounds(x - 1, y - 1) && coordinates[x - 1][y - 1] >= 2) coordinates[x - 1][y - 1] -= 2;
+            if (checkBounds(x - 1, y + 1) && coordinates[x - 1][y + 1] >= 2) coordinates[x - 1][y + 1] -= 2;
+        }
+        if(pos === 'down'){
+            if (checkBounds(x + 1, y) && coordinates[x + 1][y] >= 2) coordinates[x + 1][y] -= 2;
+            if (checkBounds(x + 1, y - 1) && coordinates[x + 1][y - 1] >= 2) coordinates[x + 1][y - 1] -= 2;
+            if (checkBounds(x + 1, y + 1) && coordinates[x + 1][y + 1] >= 2) coordinates[x + 1][y + 1] -= 2;
+        }
+        if(pos === 'left'){
+            if (checkBounds(x, y - 1) && coordinates[x][y - 1] >= 2) coordinates[x][y - 1] -= 2;
+            if (checkBounds(x + 1, y - 1) && coordinates[x + 1][y - 1] >= 2) coordinates[x + 1][y - 1] -= 2;
+            if (checkBounds(x - 1, y - 1) && coordinates[x - 1][y - 1] >= 2) coordinates[x - 1][y - 1] -= 2;
+        }
+        if(pos === 'all') {
+            if (checkBounds(x + 1, y) && coordinates[x + 1][y] >= 2) coordinates[x + 1][y] -= 2;
+            if (checkBounds(x - 1, y) && coordinates[x - 1][y] >= 2) coordinates[x - 1][y] -= 2;
+            if (checkBounds(x, y + 1) && coordinates[x][y + 1] >= 2) coordinates[x][y + 1] -= 2;
+            if (checkBounds(x, y - 1) && coordinates[x][y - 1] >= 2) coordinates[x][y - 1] -= 2;
+            if (checkBounds(x - 1, y - 1) && coordinates[x - 1][y - 1] >= 2) coordinates[x - 1][y - 1] -= 2;
+            if (checkBounds(x + 1, y - 1) && coordinates[x + 1][y - 1] >= 2) coordinates[x + 1][y - 1] -= 2;
+            if (checkBounds(x + 1, y + 1) && coordinates[x + 1][y + 1] >= 2) coordinates[x + 1][y + 1] -= 2;
+            if (checkBounds(x - 1, y + 1) && coordinates[x - 1][y + 1] >= 2) coordinates[x - 1][y + 1] -= 2;
+        }
+    }
+
+    function checkBounds(x, y) {
+        return x < 10 && x > -1 && y < 10 && y > -1
+    }
+
     return (
-        <div onMouseDown={(e) => getShip(e)}
-             onMouseUp={(e) => releasedShip(e)}
-             onKeyPress={(e) => orientationShip(e)}
-             className='container' tabIndex="-1">
-            {activeShip && <div style={{position: 'absolute',
-                left: invertedShip ? mousePos.x - shipList[activeShip-1].numberOfDecks * 22.5 : mousePos.x - 22.5 + 'px',
-                top: invertedShip ? mousePos.y + (shipList[activeShip-1].numberOfDecks - 2) * 22.5 : mousePos.y - 22.5 + 'px', pointerEvents: 'none',
-                transform: invertedShip ? 'rotate(90deg)' : 'rotate(0deg)'}}>
-                <Ship ship={shipList[activeShip-1]}/>
+        <div
+            onMouseDown={(e) => getShip(e)}
+            onMouseUp={(e) => releasedShip(e)}
+            onKeyPress={(e) => press(e)}
+            className='container' tabIndex="-1">
+            {activeShip && <div style={{
+                position: 'absolute',
+                left: invertedShip ? mousePos.x - shipList[activeShip - 1].numberOfDecks * 22.5 : mousePos.x - 22.5 + 'px',
+                top: invertedShip ? mousePos.y + (shipList[activeShip - 1].numberOfDecks - 2) * 22.5 : mousePos.y - 22.5 + 'px',
+                pointerEvents: 'none',
+                transform: invertedShip ? 'rotate(90deg)' : 'rotate(0deg)'
+            }}>
+                <Ship ship={shipList[activeShip - 1]}/>
             </div>}
             <ShipList shipList={shipList} setSelectedShip={setSelectedShip} setCoordinates={setCoordinates}
-            setShipList={setShipList} setModalActive={setModalActive} isBattleThePlayer={isBattleThePlayer} coordinates={coordinates}/>
-            <Pole numbers={numbers} letters={letters} setSelectedSquare={setSelectedSquare} placement={coordinates} shipList={shipList}/>
+                      setShipList={setShipList} setModalActive={setModalActive} isBattleThePlayer={isBattleThePlayer}
+                      coordinates={coordinates}/>
+            <Pole numbers={numbers} letters={letters} setSelectedSquare={setSelectedSquare} placement={coordinates}
+                  shipList={shipList}/>
             <Modal active={modalActive} setActive={setModalActive} coordinates={coordinates}/>
         </div>
     )
