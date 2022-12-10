dc275fd6140bb6f395fb284164b9ddd16a072920
groute63 <startref@yandex.ru>
2022-12-11 00:42:07 +0400

    бой

--- src/BattleGround/BattleTheComputer.jsx
+++ src/BattleGround/BattleTheComputer.jsx
@@ -3,6 +3,8 @@ import PoleBattle from "./componentBattle/PoleBattle";
 import classes from '../styles/PoleBattle.css'
 import {Link, useLocation, useNavigate } from 'react-router-dom';
 import {renderShipsBattle} from "../component/RenderShips";
+import {renderShipsBattleComp} from "../component/RenderShips";
+import axios from "axios";
 
 
 const BattleTheComputer = () => {
@@ -10,6 +12,7 @@ const BattleTheComputer = () => {
     const letters = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И', 'К'];
     const location = useLocation()
     const [polePlayer, setPolePlayer] = useState(location.state);
+
 
     const [poleComputer, setPoleComputer] = useState([
         [0,0,0,0,0,0,0,0,0,0],
@@ -24,23 +27,148 @@ const BattleTheComputer = () => {
         [0,0,0,0,0,0,0,0,0,0],
     ]);
 
-    const [selectedSquare, setSelectedSquare] = useState() //координаты попадания
+    const [selectedSquare, setSelectedSquare] = useState({x: null,y:null}) //координаты попадания
+    const [selectedComp, setSelectedComp] = useState({x: null,y:null});//координаты выстрела оппонента
     const [motion, setMotion] = useState(true)
     const cc = useState();
+
+    function send(){
+        axios.post(`http://localhost:8080/api/shoot`, {
+            "x": selectedSquare.y,
+            "y": selectedSquare.x
+        }).then(res => {
+            console.log(res.data)
+            if (res.data === 1) console.log('Попал')
+            if(res.data === 2) console.log('Убил')
+            if(res.data === 0) console.log("мимо");
+            if(res.data === 1) {
+                const poleComputer2 = poleComputer.slice();
+                poleComputer2[selectedSquare.y][selectedSquare.x] = 1;
+                setPoleComputer(poleComputer2);
+                setMotion(prevMotion =>true)
+            }
+            else  if(res.data === 0){
+                const poleComputer2 = poleComputer.slice();
+                poleComputer2[selectedSquare.y][selectedSquare.x] = 2;
+                setPoleComputer(poleComputer2);
+                setMotion(prevMotion =>false)
+            }
+            else  if(res.data === 2){
+                const poleComputer2 = poleComputer.slice();
+                poleComputer2[selectedSquare.y][selectedSquare.x] = 1;
+                setArea(selectedSquare.y,selectedSquare.x,poleComputer2)
+                setPoleComputer(poleComputer2);
+                setMotion(prevMotion =>true)
+            }
+
+        }).catch((error) => {
+            console.error(error.response);
+        })
+
+    }
+
+    function getCompTurn(){
+        axios.post(`http://localhost:8080/api/shootComp`, {
+
+        }).then(res => {
+            console.log(res.data)
+            let x = res.data.x;
+            let y = res.data.y;
+
+            const polePlayer2 = polePlayer.slice();
+            if(polePlayer2[x][y] === 1) {
+                polePlayer2[x][y] = -1;
+                setMotion(prevMotion =>false)
+            }
+
+            if(polePlayer2[x][y] === 0) {
+                setMotion(prevMotion =>true)
+                polePlayer2[x][y] = -2;
+            }
+
+            if(polePlayer2[x][y] >=2) {
+                setMotion(prevMotion =>true)
+                polePlayer2[x][y] = -2;
+            }
+            setPolePlayer(polePlayer2);
+
+        }).catch((error) => {
+            console.error(error.response);
+        })
+
+    }
+
+
+    function setArea(x,y,pole) {
+        setBorder(x,y,pole);
+        let n = 1;
+        while(checkBounds(x+n,y) && pole[x+n][y] === 1){
+            setBorder(x+n,y,pole)
+            n++
+        }
+        n=1;
+
+        while(checkBounds(x-n,y) && pole[x-n][y] === 1){
+            setBorder(x-n,y,pole)
+            n++
+        }
+        n=1;
+
+        while(checkBounds(x,y+n) && pole[x][y+n]=== 1){
+            setBorder(x,y+n,pole)
+            n++
+        }
+        n=1;
+
+        while(checkBounds(x,y-n) && pole[x][y-n]=== 1){
+            setBorder(x,y-n,pole)
+            n++
+        }
+    }
+
+    function checkBounds(x, y) {
+        return x < 10 && x > -1 && y < 10 && y > -1
+    }
+
+    function setBorder(x,y,pole){
+        if(checkBounds(x+1,y) && pole[x+1][y] === 0) pole[x+1][y] = 2
+        if(checkBounds(x-1,y) && pole[x-1][y] === 0) pole[x-1][y] = 2
+        if(checkBounds(x,y-1) && pole[x][y-1] === 0) pole[x][y-1] = 2
+        if(checkBounds(x,y+1) && pole[x][y+1] === 0) pole[x][y+1] = 2
+
+        if(checkBounds(x+1,y+1) && pole[x+1][y+1] === 0) pole[x+1][y+1] = 2
+        if(checkBounds(x-1,y-1) && pole[x-1][y-1] === 0) pole[x-1][y-1] = 2
+        if(checkBounds(x+1,y-1) && pole[x+1][y-1] === 0) pole[x+1][y-1] = 2
+        if(checkBounds(x-1,y+1) && pole[x-1][y+1] === 0) pole[x-1][y+1] = 2
+    }
+
     useEffect(() => {
-        console.log(selectedSquare)
-        if(selectedSquare !== undefined){
-            const poleComputer2 = poleComputer.slice();
-            //проверить попал ли игрок и изменить поле компьютера
-            /*poleComputer2[selectedSquare.y][selectedSquare.x] = 1;
-            setPoleComputer(poleComputer2)*/
-            setMotion(false)
+        if(motion === true)
+        if(selectedSquare.x !== null && poleComputer[selectedSquare.y][selectedSquare.x] !== 1 && poleComputer[selectedSquare.y][selectedSquare.x] !== 2){
+                send();
+            setMotion(prevMotion =>  false)
+
+                getCompTurn()
+            setMotion(prevMotion =>  true)
         }
     }, [selectedSquare]);
+
+    function sleep(milliseconds) {
+        const date = Date.now();
+        let currentDate = null;
+        do {
+            currentDate = Date.now();
+        } while (currentDate - date < milliseconds);
+    }
 
     useEffect(() => {
         renderShipsBattle(polePlayer)
     }, [polePlayer]);
+
+    useEffect(() => {
+        renderShipsBattleComp(poleComputer)
+    }, [poleComputer]);
+
 
     return (
         <div>
--- src/component/RenderShips.js
+++ src/component/RenderShips.js
@@ -18,33 +18,42 @@ export function renderShips(coordinates) {
 
 
 export function renderShipsBattle(coordinates) {
-    let mas =  document.getElementsByClassName('squareBattle')
-        for(let i = 0 ; i < 10; i++)
-            for(let j = 0 ; j < 10; j++) {
-                if (coordinates[i][j] === 1) {
-                    mas[i * 10 + j].className = 'squareBattle squarePlacement1';
-                }
-                if (coordinates[i][j] === 2 || coordinates[i][j] === 4 || coordinates[i][j] === 6 || coordinates[i][j] === 8) {
-                    mas[i * 10 + j].className = 'squareBattle squarePlacement2';
-                }
-                if (coordinates[i][j] === 0) {
-                    mas[i * 10 + j].style = ' background-color: white; border-color: black;'
-                }
-            }
-}
-
-export function renderShipsListPlacement(coordinates) {
-    let mas =  document.getElementsByClassName('detected')
-    for(let i = 0 ; i < 10; i++)
-        for(let j = 0 ; j < 10; j++) {
+    let mas = document.getElementsByClassName('squareBattle')
+    for (let i = 0; i < 10; i++)
+        for (let j = 0; j < 10; j++) {
             if (coordinates[i][j] === 1) {
-                mas[i * 10 + j].className = 'detected squarePlacement1';
+                mas[i * 10 + j].className += ' squarePlacement1';
             }
             if (coordinates[i][j] === 2 || coordinates[i][j] === 4 || coordinates[i][j] === 6 || coordinates[i][j] === 8) {
-                mas[i * 10 + j].className = 'detected squarePlacement2';
+                mas[i * 10 + j].className += ' squarePlacement2';
             }
             if (coordinates[i][j] === 0) {
-                mas[i * 10 + j].className = 'detected squarePlacement3'
+                mas[i * 10 + j].style = ' background-color: white; border-color: black;'
             }
+
+            if (coordinates[i][j] === -1) {
+                mas[i * 10 + j].style = ' background-color: red;'
+            }
+            if (coordinates[i][j] === -2) {
+                mas[i * 10 + j].style = ' background-color: grey;'
+            }
+
         }
 }
+
+    export function renderShipsBattleComp(coordinates) {
+        let mas = document.getElementsByClassName('square')
+
+        for (let i = 0; i < 10; i++)
+            for (let j = 0; j < 10; j++) {
+                if (coordinates[i][j] === 1) {
+                    mas[i * 10 + j].style = ' background-color: red;'
+                }
+                if (coordinates[i][j] === 2 || coordinates[i][j] === 4 || coordinates[i][j] === 6 || coordinates[i][j] === 8) {
+                    mas[i * 10 + j].style = ' background-color: grey;'
+                }
+                if (coordinates[i][j] === 0) {
+                    mas[i * 10 + j].style = ' background-color: white;'
+                }
+            }
+    }
