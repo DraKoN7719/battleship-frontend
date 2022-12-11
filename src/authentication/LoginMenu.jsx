95a3cd8d09b8a1c3232e3146f5b81e9f490a97fb
Sergey Polyakov <serega.uz@mail.ru>
2022-12-11 21:21:03 +0400

    Добавил авторизацию(заглушка - без проверки пароля, без jwt токенов)

--- src/authentication/LoginMenu.jsx
+++ src/authentication/LoginMenu.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import classes from '../style/LoginMenu.css'
 import {Link} from "react-router-dom";
+import {logoutUser} from "../Store/userReducer";
+import {useDispatch} from "react-redux";
 
 const LoginMenu = function (props) {
+    const dispatch = useDispatch()
+
+    function logout() {
+        dispatch(logoutUser());
+        localStorage.removeItem('user')
+    }
+
     return (
         <div className={'main'}>
             <img className={'logo'} src =  {require ('../style/chel.png')}/>
             <div className={'Nick'}>{props.data.name}</div>
             <Link to="/authorization">
-                <button className={'exitB'}>Выход</button>
+                <button className={'exitB'} onClick={logout}>Выход</button>
             </Link>
         </div>
     )
--- src/component/MainMenu.jsx
+++ src/component/MainMenu.jsx
@@ -3,13 +3,17 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 import LoginMenu from "../authentication/LoginMenu";
 import classes from '../style/MainMenu.css'
+import {useSelector} from "react-redux";
 
 
 
 const MainMenu = function () {
+    const loginUser = useSelector(state => state.user.login)
+
+
     return (
         <div>
-            <LoginMenu data ={{name : 'Сергей'}} />
+            <LoginMenu data ={{name : loginUser}} />
             <div className={'MainMenu'}>
                 <Link  to="/PrepareForBattle" className={'MenuText'}>
                    Играть онлайн
