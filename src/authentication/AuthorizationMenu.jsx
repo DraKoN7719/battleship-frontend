6163c7ff7656bf9f0eb55bef255d7c444e868acf
Dmitriy Savinov <drakon7719@gmail.com>
2022-12-21 06:56:13 +0400

    Проверка ввода данных при авторизации

--- src/authentication/AuthorizationMenu.jsx
+++ src/authentication/AuthorizationMenu.jsx
@@ -13,6 +13,20 @@ const AuthorizationMenu = function () {
     const [modalActive, setModalActive] = useState(false);
 
     function authorization() {
+        if (inputUserLogin.length < 2) {
+            alert("Минимальная длина логина 2 символа");
+            return;
+        } else if (inputUserLogin.length > 12) {
+            alert("Максимальная длина логина 12 символов");
+            return;
+        }
+        if (inputUserPassword.length < 4) {
+            alert("Минимальная длина пароля 4 символа");
+            return;
+        } else if (inputUserLogin.length > 12) {
+            alert("Максимальная длина пароля 12 символов");
+            return;
+        }
         axios.post(`http://${window.location.hostname}:8080/api/authorization`, {
             "login": inputUserLogin,
             "password": inputUserPassword
